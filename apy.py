aa53dcfdd11f71fa4f1bcd04aa6d77513715ec72
Andy <potterfield2005@gmail.com>
2022-01-16 17:34:42 -0600

    forgot to save

--- apy.py
+++ apy.py
@@ -8,5 +8,8 @@ url = config.endpoint
 headers = config.headers
 r = requests.request("GET", url, headers=headers)
 
+output = 'list.csv'
+message_data = []
+
 pprint.pprint(r.text)
 print(r.status_code)
