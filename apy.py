d020127f91b7980bf56ece0d8ef966372a377323
Andy <potterfield2005@gmail.com>
2022-01-16 17:34:17 -0600

    Retreives entire list of messages + status code

--- apy.py
+++ apy.py
@@ -3,3 +3,10 @@ import pprint
 import pandas as pd
 import config
 
+url = config.endpoint
+
+headers = config.headers
+r = requests.request("GET", url, headers=headers)
+
+pprint.pprint(r.text)
+print(r.status_code)
